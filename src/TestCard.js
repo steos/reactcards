4e7811f282d39a89e5713b47593bf131dd95ce9a
Stefan Oestreicher <oestef@gmail.com>
2016-05-16 00:14:45 +0200

    restyle test results.

--- src/TestCard.js
+++ src/TestCard.js
@@ -4,19 +4,55 @@ import {Card} from './components'
 const hStyle = {
   fontFamily:'sans-serif',
   fontWeight:'normal',
-  fontSize:'1.2em',
+  fontSize:'1.1em',
   margin:0,
   padding:0,
-  marginBottom:'12px',
 }
 
-const TestSuccess = props =>
-  <h4 style={{...hStyle, color:'green'}}>✔ {props.children}</h4>
+const testBoxStyle = {
+   padding:'12px',
+   borderTop:'1px solid white',
+}
+
+const CheckIcon = (props) => (
+  <svg fill={props.color} height={props.size} viewBox="0 0 24 24" width={props.size}>
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+  </svg>
+)
+
+const CloseIcon = (props) => (
+  <svg fill={props.color} height={props.size} viewBox="0 0 24 24" width={props.size}>
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+    <path d="M0 0h24v24H0z" fill="none"/>
+  </svg>
+)
+
+const iconSize = 20
+
+const TestHeader = props => (
+  <h4 style={hStyle}>
+    <span>{props.icon}</span>
+    <span style={{paddingLeft:'12px'}}>{props.children}</span>
+  </h4>
+)
+
+const TestSuccess = props => (
+  <div style={{...testBoxStyle, backgroundColor:'#DCEED3'}}>
+    <TestHeader icon={<CheckIcon color="green" size={iconSize}/>}>
+      {props.children}
+    </TestHeader>
+  </div>
+)
 
 const TestFailure = props => (
-  <div>
-    <h4 style={{...hStyle, color:'red'}}>✘ {props.children}</h4>
-    <p style={{fontFamily:'monospace'}}>{props.error.toString()}</p>
+  <div style={{...testBoxStyle, backgroundColor:'#EEDBDA'}}>
+    <TestHeader icon={<CloseIcon color="red" size={iconSize}/>}>
+      {props.children}
+    </TestHeader>
+    <div style={{fontFamily:'monospace',marginTop:'12px',marginLeft:'32px'}}>
+      {props.error.toString()}
+    </div>
   </div>
 )
 
