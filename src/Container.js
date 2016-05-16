7d88739641bc6d1182ef08aef5ea0f15bc01f073
busypeoples <busypeoples@gmail.com>
2016-05-16 22:20:43 +0200

    Added static build task + fixed route definition

--- src/Container.js
+++ src/Container.js
@@ -29,7 +29,7 @@ export default class Container extends Component {
             '#': {
                 name: 'index'
             },
-            '/#/:namespace': {
+            '#/:namespace': {
                 name: 'namespace'
             }
         })
