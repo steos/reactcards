cabb86a17d4b8e416ddcdfce0a311b0cf7746c6a
Jeff <jhicken@gmail.com>
2016-05-18 20:42:39 -0600

    fixing a bunch of spacing

--- examples/test/advanced.js
+++ examples/test/advanced.js
@@ -4,29 +4,29 @@ import { shallow } from 'enzyme'
 import { Foo, Bar } from '../components'
 
 export function testBarAdvancedComponent() {
-    describe('Test <Bar />', () => {
-        it('should display a bar. drink up!', () => {
-            const wrapper = shallow(<Bar/>)
-            assert.equal(wrapper.text(), 'a bar. drink up!')
-        })
-
-        it('should contain class bar', () => {
-            const wrapper = shallow(<Bar />)
-            assert.equal(wrapper.find('.bar').length, 1)
-        })
+  describe('Test <Bar />', () => {
+    it('should display a bar. drink up!', () => {
+      const wrapper = shallow(<Bar/>)
+      assert.equal(wrapper.text(), 'a bar. drink up!')
     })
+
+    it('should contain class bar', () => {
+      const wrapper = shallow(<Bar />)
+      assert.equal(wrapper.find('.bar').length, 1)
+    })
+  })
 }
 
 export function testFooAdvancedComponent() {
-    describe('Test <Foo />', () => {
-        it('should display Foo says \'testing\'', () => {
-            const wrapper = shallow(<Foo message="testing"/>)
-            assert.equal(wrapper.text(), "Foo says 'testing.'")
-        })
-
-        it('should contain class foo', () => {
-            const wrapper = shallow(<Foo message="testing"/>)
-            assert.equal(wrapper.find('.foo').length, 1)
-        })
+  describe('Test <Foo />', () => {
+    it('should display Foo says \'testing\'', () => {
+      const wrapper = shallow(<Foo message="testing"/>)
+      assert.equal(wrapper.text(), "Foo says 'testing.'")
     })
+
+    it('should contain class foo', () => {
+      const wrapper = shallow(<Foo message="testing"/>)
+      assert.equal(wrapper.find('.foo').length, 1)
+    })
+  })
 }
